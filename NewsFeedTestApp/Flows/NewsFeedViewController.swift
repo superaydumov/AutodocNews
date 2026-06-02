17c9bd1a35e2afcc4b4a4378235bc51ce4da8d06
superaydumov <superaydumov@me.com>
2026-06-03 01:37:08 +0300

    Initial commit. Project setup for development

--- NewsFeedTestApp/Flows/NewsFeedViewController.swift
+++ NewsFeedTestApp/Flows/NewsFeedViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class NewsFeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "News Feed Test App"
     }
-
-
 }
-
